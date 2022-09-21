48b6eb8b456981c69ae20139171415a5630dbc37
sshel <104261431+nashela@users.noreply.github.com>
2022-09-21 16:22:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/project_wibu/Home.java
@@ -0,0 +1,26 @@
+package com.example.project_wibu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+}
